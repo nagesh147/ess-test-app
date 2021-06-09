32b8b0f0ac891dfbc899bdf18c4a2589475ab10a
nageshmadaram <77847421+nageshmadaram@users.noreply.github.com>
2021-06-09 14:22:56 +0530

    one file form changes

--- src/components/Forms/EntryForm.js
+++ src/components/Forms/EntryForm.js
@@ -6,29 +6,25 @@ import data from '../../data.json'
 export default function EntryForm() {
   const {
     register,
-    handleSubmit,
-    watch,
-    formState: { errors },
   } = useForm()
 
-  const [selectedOption, setSelectedOption] = useState(null)
+  const getNextFormItem = (formItemOrder = 0) => {
+    formItemOrder++
+    console.log('formItemOrder',formItemOrder)
+    return renderElements(data.filter((i) => i.order === formItemOrder))
+  }
 
-  useEffect(() => {
-    console.log(selectedOption)
-  }, [selectedOption])
-
-  const onSubmit = (data) => console.log(data)
-
-  console.log(watch('example')) // watch input value by passing the name of it
-
-  const renderForm = (e) => {
-
+  const renderElements = (data) => {
+        console.log('Final render JSON data', data)
         return data.map(formItem => {
             return (
               <div>
                 <label>{formItem.question}</label>
                 {formItem.dataType === 'radio' ?
-                  <div>
+                  <div onChange= {(e) => 
+                    {console.log(e.target.value)
+                    e.target.value === 'Yes' && getNextFormItem(formItem.order)}
+                  }>
                     <input type={formItem.dataType} className='mbt5' value={formItem.dataTypeValue.split(',')[0]} {...register('orderOne', {required: formItem.isRequired})}/>
                     <label>{formItem.dataTypeValue.split(',')[0]}</label>
                     <br/>
@@ -38,8 +34,6 @@ export default function EntryForm() {
                   :
                   <input type={formItem.dataType} className='mbt5'  {...register('orderOne', {required: formItem.isRequired})}/>
                 }
-
-
               </div>
             )
         })
@@ -48,30 +42,8 @@ export default function EntryForm() {
 
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-      <form onSubmit={handleSubmit(onSubmit)} className='entryForm'>
-      {/* register your input into the hook by invoking the "register" function */}
-      {/* include validation with required or other standard HTML validation rules */}
-      {/* errors will return when field validation fails  */}
-
-        <label htmlFor='stepOne'>Is the leave of absence for your own injury or illness?</label>
-        <div onChange={(e)=>{ setSelectedOption(e.target.value)}} className='mbt5'>
-            <input type='radio' id='stepOneTrue' className='mbt5' value='true' {...register('stepOne', {required: true})}/>
-            <label htmlFor='stepOne'>True</label>
-            <br/>
-            <input type='radio' id='stepOneFalse' className='mbt5' value='false' {...register('stepOne', {required: true})}/>
-            <label htmlFor='stepOne'>False</label>
-        </div>
-        {selectedOption === 'true' && renderForm()}
-        
-
-
-        
-
-
-
-
-
+      <form className='entryForm'>
+       {getNextFormItem()}
     </form>
   )
 }
